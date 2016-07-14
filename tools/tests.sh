544edf5656ce37a4c8b45452e1616ff77bc13f17
WOO-CHEOL CHOI <socialpercon@gmail.com>
2016-07-14 09:40:57 +0900

    [FIX:tests] fixed test script structure

--- tools/tests.sh
+++ tools/tests.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$(cd "`dirname "$0"`"; pwd)"
-export PYTHONPATH=${CURRENT_DIR}/../src/python:${CURRENT_DIR}:${PYTHONPATH}
+echo ${CURRENT_DIR}
+export PYTHONPATH=${CURRENT_DIR}/../src/main/python:${CURRENT_DIR}/../src/tests/python:${PYTHONPATH}
 NOSE_NOCAPUTRE=1
 
 dir=.
