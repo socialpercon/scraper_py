f2552d95c38146d19f21d087307f7bf71f62de4b
WOO-CHEOL CHOI <socialpercon@gmail.com>
2016-07-21 10:41:30 +0900

    [ADD:testcode] added test case in test_mail

--- src/tests/python/tests/test_mail.py
+++ src/tests/python/tests/test_mail.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from package_name.common import mail
+import json
 
 
 def test_get_smtp_config():
     config = mail.get_smtp_config()
     print config
 
+def test_send_mail():
+    mail.send_mail(mail.get_smtp_config(), "<br>".join(["{'a':1,'b':2,'c':3}", "{'1':'a'}"]))
+
 
 if __name__ == "__main__":
-    test_get_smtp_config()
+    test_send_mail()
